a0534646e9ca24523170691422baa40b73017efd
=mohammadteeti <=>
2022-11-21 21:28:22 +0200

    add search bar and link images

--- LibraryProject/library/book_service/views.py
+++ LibraryProject/library/book_service/views.py
@@ -7,6 +7,9 @@ def home(request):
 
 def all_books(request):
     books=Book.objects.all()
+    if request.method=="POST":
+        book_name=request.POST.get("book_name","")
+        books=Book.objects.filter(title__contains=book_name)
     numOfbooks=books.count()
     
     return render(request,"book_service/all_books.html",{"books":books , "numOfbooks":numOfbooks})

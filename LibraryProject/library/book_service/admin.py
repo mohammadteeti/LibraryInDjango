7446c736bf57202acc31d600df2e89e5be4e07d3
=mohammadteeti <=>
2022-11-23 19:14:00 +0200

    add category and order models

--- LibraryProject/library/book_service/admin.py
+++ LibraryProject/library/book_service/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from book_service.models import Book
+from book_service.models import Book,Profile,Order
+from django.contrib.auth.models  import User,Group
+from django.db import models
+from django.contrib.admin import ModelAdmin
+
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.unregister(Group)
+admin.site.register(Order)
 
+class ProfileInline(admin.StackedInline):
+    model=Profile
+    
+    
+class UserAdmin(admin.ModelAdmin):
+    model=User
+    
+    inlines=[ProfileInline]
+    
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
 
--- LibraryProject/library/book_service/models.py
+++ LibraryProject/library/book_service/models.py
@@ -1,14 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 # Create your models here.
 class Book (models.Model):
     title=models.CharField(max_length=50)
     description=models.TextField()
     author=models.CharField(max_length =200)
+    image=models.FileField()
     year_of_publication=models.DateField()
     isAvailable =models.BooleanField()
     count=models.IntegerField()
     
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
+    
+class Order(models.Model):
+  books=models.ManyToManyField('Book',related_name='books')
+  ordered_on=models.DateField(auto_now_add=True)
+  
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    orderes=models.ManyToManyField('Order',related_name='orderes')
+    
+    def __str__(self):
+        return self.user.username
+  
+class Category (models.Model):
+    #Books should be in DB even after category remove
+    books = models.ForeignKey('Books',related_name='books',on_delete=models.DO_NOTHING)
+    name =models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
+
+    
+
+
 
